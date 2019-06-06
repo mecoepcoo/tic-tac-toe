30f1df37c74d1a5afc15e02a46ce4f159346f0de
tianzhen <340349237@qq.com>
2019-06-06 18:25:46 +0800

    判断胜出

--- src/index.tsx
+++ src/index.tsx
@@ -29,6 +29,7 @@ const Board: React.FC = () => {
 
   function handleClick(i: number) {
     let newSquares = squares.slice();
+    if (calculateWinner(squares) || squares[i]) return;
     newSquares[i] = xIsNext ? 'X' : 'O';
     setSquares(newSquares);
     setXIsNext(!xIsNext);
@@ -38,7 +39,14 @@ const Board: React.FC = () => {
     return <Square inputValue={squares[i]} onClick={() => handleClick(i)} />;
   };
 
-  const status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  let status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+
+  const winner = calculateWinner(squares);
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
 
   return (
     <div>
@@ -74,6 +82,26 @@ const Game: React.FC = () => {
       </div>
     </div>
   );
+}
+
+function calculateWinner(squares: Array<squareFlag>) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
