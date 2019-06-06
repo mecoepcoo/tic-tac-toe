8d0490b3203f9ca012d78ed0290f9a484fb6815e
tianzhen <340349237@qq.com>
2019-06-06 15:55:11 +0800

    添加交互

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,67 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.less';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+type SquareProps = {
+  value: number;
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Square: React.FC<SquareProps> = (props: SquareProps) => {
+  let { value } = props;
+
+  return (
+    <button className="square" onClick={function() { alert('click'); }}>
+      {value}
+    </button>
+  );
+}
+
+const Board: React.FC = () => {
+  function renderSquare(i: number) {
+    return <Square value={i} />;
+  };
+
+  const status = 'Next player: X';
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+}
+
+const Game: React.FC = () => {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  );
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
